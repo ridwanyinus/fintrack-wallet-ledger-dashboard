88ff05992caf4867cf5ebca510f5c0b6f730b46c
ridwan <ridwanyinusa15@gmail.com>
2025-08-08 11:56:29 +0100

    fix: update profile image source in navbar

--- components/ui/navigation/navbar.tsx
+++ components/ui/navigation/navbar.tsx
@@ -99,7 +99,7 @@ const Navbar = () => {
 							aria-label="User profile menu"
 						>
 							<Image
-								src="/profile.svg"
+								src="/profile1.svg"
 								alt="User profile"
 								width={40}
 								height={40}
